f81b1c562c587e2e4c15da329068d2ad25762e3a
Prakash Mohan <prakashmohan14.4@gmail.com>
2026-02-23 06:24:59 -0500

    23-02-2026

--- key-pair.tf
+++ key-pair.tf
@@ -2,5 +2,12 @@ resource "tls_private_key" "my_key" {
   algorithm = "RSA"
 }
 
-
+resource "local_file" "name" {
+  filename = "../keys/my_public_key_pem"
+  content = tls_private_key.my_key.public_key_pem
+}
+resource "local_file" "name2" {
+  filename = "../keys/my_private_key_pem"
+  content = tls_private_key.my_key.private_key_pem
+}
 
