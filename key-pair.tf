758153c709fd17c70c0c9f438afe3d6b67cd6a08
Prakash Mohan <prakashmohan14.4@gmail.com>
2026-02-24 03:27:44 -0500

    24-02-2026

--- key-pair.tf
+++ key-pair.tf
@@ -2,10 +2,6 @@ resource "tls_private_key" "my_key" {
   algorithm = "RSA"
 }
 
-resource "local_file" "name" {
-  filename = "../keys/my_public_key_pem"
-  content = tls_private_key.my_key.public_key_pem
-}
 resource "local_file" "name2" {
   filename = "../keys/my_private_key_pem"
   content = tls_private_key.my_key.private_key_pem
--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
-# resource "aws_instance" "www" {
-#   ami = var.ami
-#   instance_type = var.instance_type
-# }
+resource "aws_instance" "www" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = aws_key_pair.ec2-key.key_name
+}
 
-# resource "aws_key_pair" "ec2-key" {
-#   key_name = "my-ec2-key" 
-#   public_key = 
-# }
+resource "aws_key_pair" "ec2-key" {
+  key_name = "my-ec2-key" 
+  public_key = tls_private_key.my_key.public_key_openssh
+}
 
 
