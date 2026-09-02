cd778c3b3d523d98ef7f12e27f3ef986ca8d4bba
Prakash Mohan <prakashmohan14.4@gamil.com>
2026-09-02 20:57:47 +0530

    new commit

--- Modules/Jenkins-infra/Scripts/jenkins-install.sh
+++ Modules/Jenkins-infra/Scripts/jenkins-install.sh
@@ -7,3 +7,9 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins -y
 systemctl start jenkins.service
 systemctl enable jenkins.service
+yum install git -y
+yum install libicu -y
+cd /tmp
+wget https://dot.net/v1/dotnet-install.sh
+chmod 755 dotnet-install.sh
+sudo -u jenkins /tmp/dotnet-install.sh --version 5.0.408 --install-dir /var/lib/jenkins/.dotnet
